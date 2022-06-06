95b53aead0fc4014b1d5bcc628dde973cc9309e0
hector contreras <hectorfeboco@gmail.com>
2022-06-06 13:46:09 -0500

    Agregar archivos de proyecto.

---
+++ Data/Apiprueba1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Apiprueba1.Models;
+
+namespace Apiprueba1.Data
+{
+    public class Apiprueba1Context : DbContext
+    {
+        public Apiprueba1Context (DbContextOptions<Apiprueba1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Apiprueba1.Models.Empleado> Empleado { get; set; }
+    }
+}
---
+++ Models/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apiprueba1.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public decimal IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public decimal IdGenero { get; set; }
+        public decimal IdCuidad { get; set; }
+    }
+}
